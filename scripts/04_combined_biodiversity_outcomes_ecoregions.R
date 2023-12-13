fa7f138a10e5754fcb651f5ce3c82eabbb8931be
franciscodalbertas <francisco.albertas@gmail.com>
2023-12-13 17:43:50 -0300

    compilando resultados

--- scripts/04_combined_biodiversity_outcomes_ecoregions.R
+++ scripts/04_combined_biodiversity_outcomes_ecoregions.R
@@ -2,7 +2,6 @@
 #pacotes ------------------------------------------------------------------------
 
 library(tidyverse)
-
 #-------------------------------------------------------------------------------
 
 
@@ -11,68 +10,83 @@ library(tidyverse)
 p <- "/dados/projetos_andamento/TRADEhub/Linha_2/results"
 
 # listando tabelas
+tbls <- list.files(p,".csv",full.names = T,recursive = T)
 
-df_results <- lapply(list.files(p,".csv",full.names = T,recursive = T),read.csv)
 
-nms2split <- list.files(p,".csv",full.names = F,recursive = T)
+base <- grep(pattern = "base",tbls,value = T)
+fcnz <- grep(pattern = "fcnz",tbls,value = T)
 
-nms_splitted <- str_split(string = nms2split,pattern = "/")
+scenarios <- list(base,fcnz)
+scen_name <- c("base","fcnz")
+# loop pra salvar os resultados
+#c=1
 
-nms <- list()
 
-for(n in seq_along(nms_splitted)){
+for(i in seq_along(scenarios)){
+
+    df_results <- lapply(scenarios[[i]],read.csv)
+
+    #nms2split <- list.files(base,".csv",full.names = F,recursive = T)
+
+    nms_splitted <- str_split(string = scenarios[[i]],pattern = "/")
+
+    nms <- list()
+
+    for(n in seq_along(nms_splitted)){
+
+      nm <- nms_splitted[[n]][7]
+      nms[n] <- nm
+    }
+
+    nms <- unlist(nms)
+    names(df_results) <- nms
+
+    # adicionando nome ecorregiao
+    df_updated <- list()
+    for(df in seq_along(df_results)){
+
+      df2up <- df_results[[df]]
+      df2up$ecoregion <- names(df_results)[[df]]
+      df2up$scenario <- scen_name[i]
+      df_updated[[df]] <- df2up
+
+
+    }
+
+    # precisa incluir ecorregiao nos dados
+    # combining in one df
+
+    df_cbnd <- do.call(rbind,df_updated)
+
+    # eliminando duplicados
+
+    df_cbnd <- unique(df_cbnd)
+
+    # filtrando resultados de interesse
+
+    df_cbnd <- filter(df_cbnd ,scenario_name=="Future land-use")
+
+    # format long
+
+    clmns2keep <- grep(".val",x = names(df_cbnd))
+
+    df_long <- pivot_longer(df_cbnd,cols = c(clmns2keep,17,18,19,22))[,c(14:17)]
+
+    df_long2 <- df_long%>%
+      # criando coluna com ano (acho q tem q excluir o q nao eh future lu)
+      mutate(variable=if_else(name %in% c("VEG","AGR","PAS","area"),true="lulc","metrics"))%>%
+      # filtrando dados indesejaveis
+      filter(name!="target_value")%>%
+      mutate(year=2050)
+
+    
+
+  write.csv(df_long2,paste0("/dados/projetos_andamento/TRADEhub/Linha_2/result_tables/plangea_results_ecoregions_",scen_name[i],"_2050.csv"),row.names = F)
+
+  #  c=c+1
   
-  nm <- nms_splitted[[n]][1]
-  nms[n] <- nm
-}
-
-nms <- unlist(nms)
-
-
-
-names(df_results) <- nms
-
-# adicionando nome ecorregiao
-df_updated <- list()
-for(df in seq_along(df_results)){
   
-  df2up <- df_results[[df]]
-  df2up$ecoregion <- names(df_results)[[df]]
-  df2up$scenario <- "base"
-  df_updated[[df]] <- df2up
-}
-
-
-
-# precisa incluir ecorregiao nos dados
-
-
-# combining in one df
-
-df_cbnd <- do.call(rbind,df_updated)
-
-# eliminando duplicados
-
-df_cbnd <- unique(df_cbnd)
-
-# format long
-
-clmns2keep <- grep(".val",x = names(df_cbnd))
-
-df_long <- pivot_longer(df_cbnd,cols = clmns2keep)[,c(1,12,13,14,16,18:21)]
-
-df_long2 <- df_long%>%
-  # criando coluna com ano
-  mutate(year=if_else(scenario_name=="Future land-use",true=2050,2020))%>%
-  # filtrando dados indesejaveis
-  filter(name!="target_value")
-
-# separando values 2 c ol
-
-
-df_future <- filter(df_long2,year==2050)
-df_2020 <- filter(df_long2,year==2020)
-names(df_2020)[9] <- "value_2020"
-df_final <- cbind(df_future,df_2020[,c(9)])
+  
+  }
 
 
--- scripts/run/run_lp2_regional.R
+++ scripts/run/run_lp2_regional.R
@@ -29,41 +29,17 @@ lapply(regioes$ID,f)
 
 # piloto com 1 scenario so
 
-scen <- list.files("/dados/projetos_andamento/TRADEhub/Linha_2/rawdata/land_use/2050",pattern = "base")
+scen <- list.files("/dados/projetos_andamento/TRADEhub/Linha_2/rawdata/land_use/2050",pattern = "fcnz")
 
 #reg <- regioes$ID
 
 base <- "/dados/projetos_andamento/TRADEhub/Linha_2/rawdata/land_use/baseline_2020"
 source <- "/dados/projetos_andamento/TRADEhub/Linha_2/rawdata/land_use_regional"
 dest <- "/dados/projetos_andamento/TRADEhub/Linha_2/results/"
-# cfg$io$plangea_path = plangea_path
-
-# criando objeto com indices do JSON
-
-# oq precisa ta no base, oq precisa ta no relative e oq precisa ta no future??
-
 
 cfg = jsonlite::fromJSON("/dados/pessoal/francisco/Linha_2_TradeHub/scripts/JSON/L2_ecoregions.json")
 
-#excluir <-frequencia$Freq[frequencia$Freq<862]
-#excluir <- c("13","257","266","68","208")
-#reg <- reg[!reg %in% excluir]
-# rodar faltantes
-#reg <- reg[38:66]
-
-#jafoi
-#foi <- c(1,102,11:16,168,17,18,19,2,22,23,291,292,293,294,3,301,326,340,39,4,406,48,5,58,59,6,68,7,8,86,87,9,94)
-
-# checando quais falam
-
-# falta <- list.files("/dados/projetos_andamento/TRADEhub/Linha_2/results",recursive = T,pattern = "csv")
-
-
-
-#reg_inedito <- reg[!reg %in% foi]
-#reg <- reg_inedito
-reg <-c(358,420,421,427,452,469,500,555,557,566,567,579,580,598,623,640,676,677,690,727,761,766,803,820,836)
-# tem q adiconar o lugar pra salvar
+reg <- regioes$ID
 
 for(i in seq_along(reg)){
   for(j in 1:length(scen)){
@@ -84,24 +60,3 @@ for(i in seq_along(reg)){
   
 }
 
-# parou no 257 nao sei pq: erro:
-
-#Error in harmonize_pa(cfg, file_log = harmonize_log, verbose = verbose,  : 
-# Computed PA have NA values in the master_index
-# Calls: .rs.sourceWithProgress ... eval -> eval -> plangea -> harmonize -> harmonize_pa
-
-# agora parou no 326
-
-# deu errado a mascara ai
-# ecoregions <- rast("/dados/projetos_andamento/TRADEhub/Linha_2/rawdata/subregions/ecoregions_wwf_plusCerrado.tif")
-# 
-# legenda <- read.csv("/dados/projetos_andamento/TRADEhub/Linha_2/rawdata/subregions/dicionario_Cerrado_wwf.csv")
-# 
-# eco257 <- ecoregions==677
-# plot(eco257)
-# unique(values(eco257))
-# frequencia <- as.data.frame(table(values(ecoregions)))
-# 
-# # tem q ver os q tem freq. mto baixa!
-# # usar isso pra filtrar as regioes
-# 
