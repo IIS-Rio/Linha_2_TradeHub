0ebbbb23eed9ca5c1ccf615a5c40c1bb0cadba5f
franciscodalbertas <francisco.albertas@gmail.com>
2023-12-14 17:46:18 -0300

    analise qualitativa dos resultados

---
+++ scripts/figures/outcome_scenarios_bd.R
@@ -0,0 +1,155 @@
+library(scales)
+library(ggpubr)
+library(ggmap)
+# analise exploratoria resultado scenarios
+
+# caminho
+
+p <- "/dados/projetos_andamento/TRADEhub/Linha_2/result_tables"
+
+# listando tabelas
+
+lst_tbls <- list.files(p,full.names = T)
+
+# abrindo scenarios
+
+base <-read.csv(lst_tbls[grep("base",lst_tbls)])
+fcnz <- read.csv(lst_tbls[grep("fcnz",lst_tbls)])
+
+# filtrando apenas dados completos
+
+base <- base[base$ecoregion%in% fcnz$ecoregion,]
+
+# calcular % em relacao a 2050 ou 2020? por enquanto, 2050
+
+
+# comparando metricas ----------------------------------------------------------
+
+base_m <- filter(base,variable=="metrics")
+fcnz_m <- filter(fcnz,variable=="metrics")
+
+# cenario fcnz em funcao do base
+
+fcnz_relative <- left_join(fcnz_m,base_m[,c(1,3,4)],by=c("ecoregion","name"))%>%
+  dplyr::rename(value_base_2050=value.y,value=value.x)%>%
+          #limpa na
+          filter(!is.na(value))%>%
+          # foca so em biodiv%>%
+          filter(!name %in% c("cb.val","oc.val"))%>%
+          # calculating ratio
+          mutate(ratio=(value/value_base_2050),
+                 inv_ratio=1-ratio)# invertendo pra mostrar onde eh pior no cenario nao-base
+
+
+# as metricas sao de reducao. qnto menor a reducao, pior, logo qnto menor o valor do ratio, pior!
+
+# abrindo raster ecoregioes
+
+ecoregion <- rast("/dados/projetos_andamento/TRADEhub/Linha_2/rawdata/subregions/ecoregions_wwf_plusCerrado.tif") 
+
+ecoregion_vec <- as.polygons(ecoregion)
+
+bd <- filter(fcnz_relative,name=="bd.val")
+
+ecoregion_vec <- st_as_sf(ecoregion_vec)
+ecoregion_vec <- st_as_sf(left_join(bd,ecoregion_vec,by = join_by(ecoregion==focal_modal) ))
+
+
+# ratio > signfica que base>scen, logo, a reducao eh maior no base!
+# pra mostrar onde eh pior no scen, teria q inverter entao!
+# valores negativos ou positivos nao tem siginificado. so maior ou menor importa. 
+
+# qndo base eh > q fcnz?
+# qndo os valores do ratio sao negativos ate menor q 1 positivo!
+
+# Define a color palette
+# Define a custom color palette
+#bd_rast <- rasterize(vect(ecoregion_vec),ecoregion,field="ratio")
+# bd_rast2 <- rasterize(vect(ecoregion_vec),ecoregion,field="ratio")
+
+library(RColorBrewer)
+
+# Specify the number of shades of blue you want
+
+num_shades_blue <- length(unique(df$ratio[df$ratio<1]))
+num_shades_red <- length(unique(df$ratio[df$ratio>1]))
+# Generate a range of blue color codes
+# a qntidade de cores muda a legenda, e ferra a transicao.
+
+blue_palette <- colorRampPalette(c("lightblue", "darkblue"))(num_shades_blue)
+red_palette <- colorRampPalette(c("orange", "darkred"))(num_shades_red)
+
+blue_palette_legend <- colorRampPalette(c("lightblue", "darkblue"))(20)
+red_palette_legend <- colorRampPalette(c("orange", "darkred"))(4)
+
+color_df_blues <- data.frame(unique_vals=unique(df$ratio[df$ratio<1]))%>%
+    arrange(unique_vals)%>%
+    mutate(col_code=rev(blue_palette))
+
+color_df_red <- data.frame(unique_vals=unique(df$ratio[df$ratio>1]))%>%
+  arrange(unique_vals)%>%
+  mutate(col_code=(red_palette))
+
+#combine
+
+color_df <- rbind(color_df_blues,color_df_red)
+
+# tentar plotar como shape mesmo (mais rapido!)
+
+# da pra fazer um painel por cenario, e com cada metrica!!
+
+color_df$unique_vals <- as.numeric(color_df$unique_vals)
+color_df$ID <- round(color_df$unique_vals,3)
+
+ecoregion_vec$ID <- round(ecoregion_vec$ratio,3)
+
+ecoregion_vec2 <- left_join(ecoregion_vec,color_df)
+
+# brazil countours
+
+Br <- read_country(2019)%>%
+  mutate(dissolve=1)%>%
+  group_by(dissolve)%>%
+  summarise()%>%
+  st_transform(st_crs(ecoregion_vec2))
+
+biomes <- st_read("/dados/projetos_andamento/TRADEhub/Linha_2/other_covariables/biomes.shp")
+
+
+plot_fcnz <- ggplot() +
+  geom_sf(data = Br, color = "black", fill = NA) +
+  geom_sf(data = ecoregion_vec2, aes(fill = col_code), color = "transparent") +
+  geom_sf(data = biomes, color = "black",fill=NA)+
+  scale_fill_identity() +
+  #scale_fill_manual(values = ecoregion_vec2$col_code ) +
+  #ggtitle("fcnz")+
+  labs( title = "fcnz", fill = "Legend Title" ) +
+  #guides(fill = guide_legend(title = "Ratio Legend"))+
+  theme_map()
+
+# tem q ser isso pra legenda, e o de cima pra plotar!
+legend_fcnz <- ggplot() +
+  geom_sf(data = Br, color = "black", fill = NA) +
+  geom_sf(data = ecoregion_vec2, aes(fill = ratio), color = "transparent") +
+  scale_fill_gradientn(colours = 
+    c(blue_palette_legend, red_palette_legend),
+    limits = range(ecoregion_vec2$ratio),
+    name = "Relative change",
+    breaks = c(-2.5,0, 1)
+  ) +
+  labs(
+    title = "fcnz",
+    fill = "Legend Title"
+  ) +
+  theme_map()
+
+legend2plot <- get_legend(legend_fcnz,position = "left")
+plot(legend2plot)
+
+# isso aqui ta ficando melhor
+arranged_plot <- ggarrange(plot_fcnz,plot_fcnz,plot_fcnz,legend2plot,ncol=4,heights = c(1,1,1,0.5),widths = c(1,1,1,0.5))
+
+# Add the title
+final_bd <- annotate_figure(arranged_plot , top = text_grob("Extinction Debt",color = "black", face = "bold", size = 12))
+
+ggsave(plot = plot,filename = "/dados/pessoal/francisco/Linha_2_TradeHub/figures/outcome_bd_fcnz_relative_baseline2050.png",height = 10,width = 10,units = "cm",dpi=100)
--- scripts/run/run_lp2_regional.R
+++ scripts/run/run_lp2_regional.R
@@ -16,8 +16,8 @@ p <- "/dados/projetos_andamento/TRADEhub/Linha_2/results"
 
 regioes <- read.csv("/dados/projetos_andamento/TRADEhub/Linha_2/rawdata/subregions/dicionario_Cerrado_wwf.csv")
 
-f <- function(x)dir.create(file.path(p,x))
-lapply(regioes$ID,f)
+# f <- function(x)dir.create(file.path(p,x))
+# lapply(regioes$ID,f)
 
 
 # configurando o JSON pra rodar pra varias regioes
@@ -41,22 +41,50 @@ cfg = jsonlite::fromJSON("/dados/pessoal/francisco/Linha_2_TradeHub/scripts/JSON
 
 reg <- regioes$ID
 
-for(i in seq_along(reg)){
-  for(j in 1:length(scen)){
-    
-    # definir aqui. testar ser results
-    cfg$io$base_path <- paste0(dest,reg[i],"/",scen[j],"/") #fazer so pra 1 regiao pra ver
-    
-    # aqui seria o caminho uso presente
-    cfg$io$lu_relative_path <- paste0("land_use_regional/ecoregion_",reg[i],"/")
-    
-    
-    # aqui uso futuro
-    cfg$io$future_lu_relative_path <- paste0("land_use/2050/",scen[j],"/")
-    
-    plangea(cfg = cfg)    
-    
-  }
+# definir regioes para serem excluidas com base na area mto pequena
+# parou na 208
+
+excluir <- c(13,257,266,68,208,677,326) 
+
+reg <- reg[!reg %in% excluir]
+
+# nao rodar de novo as que ja foram
+
+reg2 <- reg[which(reg==291):60]
+
+# definindo tasks
+
+tasks <- expand.grid(reg,scen)
+names(tasks) <- c("reg","scen")
+
+plangea_run <- function(reg,scen, dest, cfg){
   
+      # definir aqui. testar ser results
+      cfg$io$base_path <- paste0(dest,reg,"/",scen,"/") #fazer so pra 1 regiao pra ver
+  
+      # aqui seria o caminho uso presente
+      cfg$io$lu_relative_path <- paste0("land_use_regional/ecoregion_",reg,"/")
+  
+  
+      # aqui uso futuro
+      cfg$io$future_lu_relative_path <- paste0("land_use/2050/",scen,"/")
+  
+      plangea(cfg = cfg)
+
 }
 
+
+# run in parallel
+
+num_clusters <- 10
+
+cl <- makeCluster(num_clusters)
+
+
+# Run tasks in parallel
+foreach(i = 1:nrow(tasks), .combine = 'c') %dopar% {
+  plangea_run(reg = tasks$reg[i], scen = tasks$scen[i] ,dest, cfg)
+}
+
+# Stop the parallel cluster
+stopCluster(cl)
